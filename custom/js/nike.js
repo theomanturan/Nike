3580407540fa598228dd0d67ac2c1b0503c6e8c7
theomanturan <theoman.turan@hotmail.fr>
2024-12-09 09:37:29 +0000

    nike starter kit

---
+++ custom/js/nike.js
@@ -0,0 +1,31 @@
+// liste de courses : modulo, innerHTML, dataset
+
+const settings = {
+    itemsPerPage: 6,
+    shoes: [
+        { name: "Air Jordan 1", price: 26780, image: "jordan-1.webp" },
+        { name: "Air Jordan 2", price: 7680, image: "jordan-2.webp" },
+        { name: "Air Jordan 3", price: 26543, image: "jordan-3.webp" },
+        { name: "Air Jordan 4", price: 14900, image: "jordan-4.webp" },
+        { name: "Air Jordan 5", price: 4780, image: "jordan-5.webp" },
+        { name: "Air Jordan 6", price: 18980, image: "jordan-6.webp" },
+        { name: "Air Jordan 7", price: 26780, image: "jordan-6.webp" },
+        { name: "Air Jordan 8", price: 7680, image: "jordan-5.webp" },
+        { name: "Air Jordan 9", price: 26543, image: "jordan-4.webp" },
+        { name: "Air Jordan 10", price: 14900, image: "jordan-3.webp" },
+        { name: "Air Jordan 11", price: 4780, image: "jordan-2.webp" },
+        { name: "Air Jordan 12", price: 18980, image: "jordan-1.webp" },
+        { name: "Air Jordan 13", price: 26780, image: "jordan-1.webp" },
+        { name: "Air Jordan 14", price: 7680, image: "jordan-2.webp" },
+        { name: "Air Jordan 15", price: 26543, image: "jordan-3.webp" },
+        { name: "Air Jordan 16", price: 14900, image: "jordan-4.webp" },
+        { name: "Air Jordan 17", price: 4780, image: "jordan-5.webp" },
+        { name: "Air Jordan 18", price: 18980, image: "jordan-6.webp" },
+        { name: "Air Jordan 19", price: 26780, image: "jordan-6.webp" },
+        { name: "Air Jordan 20", price: 7680, image: "jordan-5.webp" },
+        { name: "Air Jordan 21", price: 26543, image: "jordan-4.webp" },
+        { name: "Air Jordan 22", price: 14900, image: "jordan-3.webp" },
+        { name: "Air Jordan 23", price: 4780, image: "jordan-2.webp" },
+        { name: "Air Jordan 24", price: 18980, image: "jordan-1.webp" },
+    ]
+};
